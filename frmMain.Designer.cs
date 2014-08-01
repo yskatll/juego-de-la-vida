407e95df56b4bcc158ae76c8857b9fd71202a5ee
Miguel Yskatll <yskatll@gmail.com>
2014-08-01 04:29:35 -0500

    juego-de-la-vida

---
+++ frmMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Juego_de_la_Vida
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnBloque = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBarca = new System.Windows.Forms.Button();
+            this.btnPortaa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tmrLoop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIniciar.Image = ((System.Drawing.Image)(resources.GetObject("btnIniciar.Image")));
+            this.btnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.Location = new System.Drawing.Point(623, 12);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(85, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDetener.Enabled = false;
+            this.btnDetener.Image = ((System.Drawing.Image)(resources.GetObject("btnDetener.Image")));
+            this.btnDetener.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDetener.Location = new System.Drawing.Point(623, 41);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(85, 23);
+            this.btnDetener.TabIndex = 1;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnBloque
+            // 
+            this.btnBloque.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBloque.Location = new System.Drawing.Point(623, 115);
+            this.btnBloque.Name = "btnBloque";
+            this.btnBloque.Size = new System.Drawing.Size(85, 23);
+            this.btnBloque.TabIndex = 2;
+            this.btnBloque.Text = "Bloque";
+            this.btnBloque.UseVisualStyleBackColor = true;
+            this.btnBloque.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(620, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Plantillas";
+            this.label1.Visible = false;
+            // 
+            // btnBarca
+            // 
+            this.btnBarca.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBarca.Location = new System.Drawing.Point(623, 144);
+            this.btnBarca.Name = "btnBarca";
+            this.btnBarca.Size = new System.Drawing.Size(85, 23);
+            this.btnBarca.TabIndex = 4;
+            this.btnBarca.Text = "Barca";
+            this.btnBarca.UseVisualStyleBackColor = true;
+            this.btnBarca.Visible = false;
+            // 
+            // btnPortaa
+            // 
+            this.btnPortaa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPortaa.Location = new System.Drawing.Point(623, 173);
+            this.btnPortaa.Name = "btnPortaa";
+            this.btnPortaa.Size = new System.Drawing.Size(85, 23);
+            this.btnPortaa.TabIndex = 5;
+            this.btnPortaa.Text = "Portaaviones";
+            this.btnPortaa.UseVisualStyleBackColor = true;
+            this.btnPortaa.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(620, 525);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 65);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Desarrollado por:\r\nMiguel Izcatl\r\n\r\nPara:\r\nCrowd Interactive";
+            // 
+            // tmrLoop
+            // 
+            this.tmrLoop.Interval = 2000;
+            this.tmrLoop.Tick += new System.EventHandler(this.tmrLoop_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(720, 599);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPortaa);
+            this.Controls.Add(this.btnBarca);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBloque);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnIniciar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Juego de la Vida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnBloque;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBarca;
+        private System.Windows.Forms.Button btnPortaa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmrLoop;
+
+
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_de_la_Vida
+{
+    public partial class frmMain : Form
+    {
+        int numeroCelulas = 30;
+        int tamañoCelula = 20;
+        System.Drawing.Graphics g;
+        bool[,] celula =  new bool[30, 30];
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //inicializar todas las celulas como muertas x_x
+            for (int x = 0; x < numeroCelulas; x++)
+            {
+                for (int y = 0; y < numeroCelulas; y++)
+                {
+                    celula[x, y] = false;
+                }
+            }
+        }
+
+        private void nuevaGeneracion()
+        {
+            bool[,] celulaNueva = new bool[numeroCelulas, numeroCelulas];
+            int numVecinos;
+            for (int x = 0; x < numeroCelulas; x++)
+            {
+                for (int y = 0; y < numeroCelulas; y++)
+                {
+                    celulaNueva[x, y] = celula[x, y];
+                    numVecinos = contarVecinos(x, y);
+                    if (numVecinos < 2 || numVecinos > 3) //muere
+                    {
+                        celulaNueva[x, y] = false;
+                    }
+                    if (celulaNueva[x, y] == false && numVecinos == 3) //nace
+                    {
+                        celulaNueva[x, y] = true;
+                    }
+                    if (celulaNueva[x, y] == true && (numVecinos == 2 || numVecinos == 3)) //sobrevive
+                    {
+                        celulaNueva[x, y] = true;
+                    }
+                }
+            }
+            celula = celulaNueva;
+            pintarCelulas();
+        }
+
+        private int contarVecinos(int x, int y)
+        {
+            int vecinos = 0;
+            for (int i = x - 1; i <= x + 1; i++) //desde la columna actual menos uno, hasta la columna actual mas uno
+            {
+                for (int j = y - 1; j <= y + 1; j++) //desde la fila actual menos uno, hasta la fila actual mas uno
+                {
+                    if (!(x == i && y == j) && i >= 0 && j >= 0 && i < numeroCelulas && j < numeroCelulas) //para evitar contarn a la celula actual, y no salirnos de los bordes
+                    {
+                        if (celula[i, j] == true)
+                        {
+                            vecinos++; //sumamos los vecinos
+                        }
+                    }
+                }
+            }
+            return vecinos;
+        }
+
+        private void pintarCelulas()
+        {
+            g = this.CreateGraphics();
+            for (int x = 0; x < numeroCelulas; x++)
+            {
+                for (int y = 0; y < numeroCelulas; y++)
+                {
+                    if (celula[x, y]) //si la celula esta viva...
+                    {
+                        //dibujamos un cuadro
+                        Rectangle rect =
+                                new Rectangle((tamañoCelula * x),
+                                              (tamañoCelula * y),
+                                              tamañoCelula,
+                                              tamañoCelula);
+                        g.FillRectangle(Brushes.SlateGray, rect);
+                    }
+                    else
+                    {
+                        //borramos el cuadro
+                        Rectangle rect =
+                                new Rectangle((tamañoCelula * x),
+                                              (tamañoCelula * y),
+                                              tamañoCelula,
+                                              tamañoCelula);
+                        g.FillRectangle(Brushes.White, rect);
+                    }
+                }
+            }
+            frmMain_Paint(null, null); //redibujamos las lineas del grid
+            g.Dispose();
+        }
+
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            g = this.CreateGraphics();
+            //pintamos las lineas del grid
+            for (int i = 0; i < numeroCelulas; i++)
+            {
+                //vertical
+                g.DrawLine(Pens.SlateGray, i * tamañoCelula, 0, i * tamañoCelula, numeroCelulas * tamañoCelula);
+                //horizontal
+                g.DrawLine(Pens.SlateGray, 0, i * tamañoCelula, numeroCelulas * tamañoCelula, i * tamañoCelula);
+            }
+            //ultima linea
+            g.DrawLine(Pens.SlateGray, numeroCelulas * tamañoCelula, 0, numeroCelulas * tamañoCelula, numeroCelulas * tamañoCelula);
+            g.Dispose();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Enabled = false;
+            btnDetener.Enabled = true;
+            //iniciamos el timer para crear nuevas generaciones de celulas
+            tmrLoop.Enabled = true;
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Enabled = true;
+            btnDetener.Enabled = false;
+            //detenemos el timer, esto permite q insertemos nuevas celulas manualmente
+            tmrLoop.Enabled = false;
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            g = this.CreateGraphics();
+            int celulaX = 0;
+            int celulaY = 0;
+            //verificar q no hagan clic fuera del grid
+            if (e.Location.X > 0 & e.Location.X < 600 & e.Location.Y > 0 & e.Location.Y < 635)
+            {
+                //obtener la ubicacion de la celula
+                celulaX = Convert.ToInt32(Math.Truncate(Convert.ToDouble(e.Location.X) / tamañoCelula));
+                celulaY = Convert.ToInt32(Math.Truncate(Convert.ToDouble(e.Location.Y) / tamañoCelula));
+                //marcar la celula como viva
+                celula[celulaX, celulaY] = true;
+                //dibujamos un cuadro
+                Rectangle rect =
+                        new Rectangle((tamañoCelula * celulaX),
+                                      (tamañoCelula * celulaY),
+                                      tamañoCelula,
+                                      tamañoCelula);
+                g.FillRectangle(Brushes.SlateGray, rect);
+                g.Dispose();
+            }
+        }
+
+        private void tmrLoop_Tick(object sender, EventArgs e)
+        {
+            nuevaGeneracion();
+        }
+    }
+}
